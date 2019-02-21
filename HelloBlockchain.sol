924d95da64615874dad01928aecaca93a694dc8a
Senthamil <senthamilv@outlook.com>
2019-02-21 18:09:01 +0800

    Add files via upload

---
+++ HelloBlockchain.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.25;
+
+contract HelloBlockchain
+{
+     //Set of States
+    enum StateType { Request, Respond}
+
+    //List of properties
+    StateType public  State;
+    address public  Requestor;
+    address public  Responder;
+
+    string public RequestMessage;
+    string public ResponseMessage;
+
+    // constructor function
+    constructor(string message) public
+    {
+        Requestor = msg.sender;
+        RequestMessage = message;
+        State = StateType.Request;
+    }
+
+    // call this function to send a request
+    function SendRequest(string requestMessage) public
+    {
+        if (Requestor != msg.sender)
+        {
+            revert();
+        }
+
+        RequestMessage = requestMessage;
+        State = StateType.Request;
+    }
+
+    // call this function to send a response
+    function SendResponse(string responseMessage) public
+    {
+        Responder = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = responseMessage;
+        State = StateType.Respond;
+    }
+}
